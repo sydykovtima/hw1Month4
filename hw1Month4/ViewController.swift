c9124f08ec38c76c880306b0552a8a0085ae27a1
tima <timka.0416@gmail.com>
2023-01-26 14:21:13 +0600

    Initial Commit

---
+++ hw1Month4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw1Month4
+//
+//  Created by Mac on 26/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
